8cf05fd4b103b34e0bee375cf0be345bce784ee3
Oxyenniu <Oxyenniu@gmail.com>
2022-10-29 12:26:03 +0300

    Initial Commit

---
+++ ThirdColorizedApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ThirdColorizedApp
+//
+//  Created by Oxyenniu on 29.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
